239694dfbba4da1bf69b49ce9a8052be507fd287
Emmanuel <aboyejiemmanuel07@gmail.com>
2024-07-03 13:09:05 +0100

    update admin priviledge controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -485,6 +485,30 @@ export const superAdminAddAdmin = async (req, res) => {
   }
 };
 
+export const updateAdminPriviledge = async (req, res) => {
+  try {
+    const { adminId } = req.params;
+    const { priviledges } = req.body;
+
+    if (!Array.isArray(priviledges)) {
+      return res.status(400).json({ message: "Privileges must be an array" });
+    }
+
+    const updatedAdmin = await Admin.findOneAndUpdate(
+      { adminId },
+      { priviledges },
+      { new: true } // This option returns the modified document
+    );
+
+    if (!updatedAdmin) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+
+    res.status(200).json({ message: "Admin privileges updated successfully", updatedAdmin });
+  } catch (err) {
+    res.status(500).json({ message: "Error updating admin privileges", err });
+  }
+};
 // User logout (Blacklisting the JWT)
 export const logout = async (req, res) => {
   try {
